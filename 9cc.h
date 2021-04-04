31c46d0fa82c43d29b44af4ed6f14f4527cb5f7c
Taketoshi Fujiwara <taketoshi.fujiwara@gmail.com>
2021-04-04 20:08:13 +0900

    split files

---
+++ 9cc.h
@@ -0,0 +1,69 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+  ND_EQ,
+  ND_NE,
+  ND_LE,
+  ND_LT,
+} NodeKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+  int len;
+};
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool at_eof();
+
+Token *tokenize();
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+void gen(Node *node);
+
+extern Token *token;
+extern char *user_input;
---
+++ main.c
@@ -0,0 +1,22 @@
+#include "9cc.h"
+
+Token *token;
+char *user_input;
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    error("incorrect number of argument");
+  }
+
+  user_input = argv[1];
+  token = tokenize();
+  Node *node = expr();
+
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+  gen(node);
+  printf("  pop rax\n");
+  printf("  ret\n");
+  return 0;
+}
